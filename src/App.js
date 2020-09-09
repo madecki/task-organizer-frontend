c52185d038192e6a12fedd445e324904fb467fe7
Krzysztof Mendrek <kmendrek94@gmail.com>
2020-09-09 15:33:28 +0200

    Routing has been prepared

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-route-dom';
 import './App.css';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <>
+    <Router>
+      <Switch>
+        <Route></Route>
+      </Switch>
+    </Router>
+    </>
+  )
 }
 
 export default App;

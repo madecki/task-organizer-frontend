f96508d0f367fcc8ef420709bfb2ff0b2188e4e1
Krzysztof Mendrek <kmendrek94@gmail.com>
2020-09-10 13:54:25 +0200

    pull and merge done with feature / to-2 in routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-<<<<<<< HEAD
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-route-dom";
-import "./App.css";
-=======
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-route-dom';
 import './App.css';
->>>>>>> feature/to-2-preperation-to-work
 
 function App() {
   return (
     <>
       <Router>
         <Switch>
-          <Route></Route>
+          <Route />
         </Switch>
       </Router>
     </>

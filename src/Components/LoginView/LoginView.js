45f6cf6709f3e889b68d6cd9a9ddb09f3a955fdf
Krzysztof Mendrek <kmendrek94@gmail.com>
2020-09-11 11:16:56 +0200

    Basic components has been created

---
+++ src/Components/LoginView/LoginView.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './LoginView.css';
+
+function LoginView() {
+  return ();
+}
+
+export default LoginView;
---
+++ src/Components/RegistrationView/RegistrationView.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './RegistrationView.css';
+
+function RegistrationView() {
+  return ();
+}
+
+export default RegistrationView;

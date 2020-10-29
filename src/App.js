ed2da6f5a0ebf614a7df81230f4aae7fc43e6049
Mateusz Rajek <mateusz_rajek@wp.pl>
2020-10-29 14:51:17 +0000

    installed fontawesome far library; added iconSwitch function to Task.js component, that change task status icon; added 'add task' button to TaskView design

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import './App.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab, faFacebookF, faGoogle } from '@fortawesome/free-brands-svg-icons';
 import { fas, faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons'
 import MainView from './Components/MainView/MainView';
 import LoginView from './Components/LoginView/LoginView';
 
-library.add(fab, fas, faEnvelope, faLock, faFacebookF, faGoogle);
+library.add(far, fab, fas, faEnvelope, faLock, faFacebookF, faGoogle);
 
 function App() {
   return (

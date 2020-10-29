31ebb0b5ec089611b7cba140a2940887c61c3572
krzysztofmendrek <kmendrek@o2.pl>
2020-10-29 10:34:28 +0100

    Fixed problem in case of no geolocation

--- src/Components/ForecastView/ForecastView.js
+++ src/Components/ForecastView/ForecastView.js
@@ -2,17 +2,25 @@ import React, { useState, useEffect } from 'react';
 import { format, getHours, getISODay, getMinutes } from 'date-fns';
 import getWeatherByCoordinates from '../../request';
 import './ForecastView.css';
+import warning from '../../Assets/Icon/warning.svg';
 
 function ForecastView() {
   const [weatherInfo, setWeatherInfo] = useState('');
+  const [isGeolocation, setGeolocation] = useState(false);
   const currentTime = () => {
     const hour = getHours(new Date());
     const minutes = getMinutes(new Date());
     return minutes.toString().length === 1 ? `${hour}:0${minutes}` : `${hour}:${minutes}`;
   };
   const [time, setTime] = useState(currentTime());
+  const dayOfWeek = date => {
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const day = days[getISODay(date)];
+    return day;
+  };
 
   function success(pos) {
+    setGeolocation(false);
     const crd = pos.coords;
     const { latitude } = crd;
     const { longitude } = crd;
@@ -25,6 +33,7 @@ function ForecastView() {
         desc: `${data.weather[0].description} icon`
       };
       setWeatherInfo(listOfWeatherInfo);
+      setGeolocation(true);
     });
   }
 
@@ -32,32 +41,33 @@ function ForecastView() {
     setInterval(() => {
       setTime(currentTime());
     }, 60000);
-  }, []);
-
-  const dayOfWeek = date => {
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const day = days[getISODay(date)];
-    return day;
-  };
-
-  useEffect(() => {
     navigator.geolocation.getCurrentPosition(success);
   }, []);
 
   return (
-    <div className='forecast__container'>
-      <h2 className='forecast__city'>{weatherInfo.city}</h2>
-      <div className='hour-and-weather-wrapper'>
-        <p className='forecast__hour'>{time}</p>
-        <div className='forecast__weather'>
-          <img src={weatherInfo.icon} alt={weatherInfo.desc} />
-          <p>{weatherInfo.temp}°C</p>
+    <>
+      {isGeolocation && (
+        <div className='forecast__container'>
+          <h2 className='forecast__city'>{weatherInfo.city}</h2>
+          <div className='hour-and-weather-wrapper'>
+            <p className='forecast__hour'>{time}</p>
+            <div className='forecast__weather'>
+              <img src={weatherInfo.icon} alt={weatherInfo.desc} />
+              <p>{weatherInfo.temp}°C</p>
+            </div>
+          </div>
+          <p className='forecast__date'>
+            {`${format(new Date(), 'dd.MM.yyyy')} | ${dayOfWeek(new Date())}`}
+          </p>
         </div>
-      </div>
-      <p className='forecast__date'>
-        {`${format(new Date(), 'dd.MM.yyyy')} | ${dayOfWeek(new Date())}`}
-      </p>
-    </div>
+      )}
+      {!isGeolocation && (
+        <div className='forecast__container forecast__container--warning'>
+          <img src={warning} alt='warning icon' />
+          <p>You do not allow the location, please change it to use the weather forecast</p>
+        </div>
+      )}
+    </>
   );
 }
 
